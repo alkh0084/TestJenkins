6587c07919b01bd5a2bf53de7d40027ca0a8ce50
alkh0084 <alkh0084@algonquinlive.com>
2019-11-04 03:22:43 +0000

    ssdfsdfdsf

--- target/metamodel/com/algonquincollege/cst8277/models/Project_.java
+++ target/metamodel/com/algonquincollege/cst8277/models/Project_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2019-11-03T20:52:07")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2019-11-03T21:12:00")
 @StaticMetamodel(Project.class)
 public class Project_ extends ModelBase_ {
 
